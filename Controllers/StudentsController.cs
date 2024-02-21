b9e33f7dfb2aa71fb2791da5630e42267be3281b
Pedro Lima <tsc.pedrolima@gmail.com>
2024-02-21 15:27:51 -0300

    feat: add delete button

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -71,5 +71,21 @@ namespace StudentPortal.Web.Controllers
 
             return RedirectToAction("List", "Students");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == viewModel.Id);
+
+            if (viewModel is not null)
+            {
+                dbContext.Students.Remove(viewModel);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+        }
     }
 }
