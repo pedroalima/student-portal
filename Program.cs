eda1ecbec2b4140b80be4fde5d12e67723db8f32
Pedro Lima <tsc.pedrolima@gmail.com>
2024-02-27 21:31:57 -0300

    style: remove home and privacy route

--- Program.cs
+++ Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Students}/{action=List}/{id?}");
 
 app.Run();
